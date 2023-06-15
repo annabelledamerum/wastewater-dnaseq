ad2084ffb1ad95851fe46dcdf1ce4dad329ad846
nsharp2 <nsharp@zymoresearch.com>
2023-06-15 11:52:13 -0700

    read sample cutoff changed to user-input parameter (default: 1 million)

--- bin/low_read_filter.py
+++ bin/low_read_filter.py
@@ -4,12 +4,12 @@ import numpy as np
 import pandas as pd
 import argparse
 
-def qiime_taxmerge(qza, readcount):
+def qiime_taxmerge(qza, readcount, lowread_filter):
     totalreads = pd.read_csv(readcount, index_col=0)
-    exclude = totalreads[totalreads["aligned"] < 1000000].index
+    exclude = totalreads[totalreads["aligned"] < lowread_filter].index
     for i in exclude:
         qza = re.sub(i+"_qiime_absfreq_table.qza", "", qza)
-    subset_num = totalreads[totalreads["aligned"] >= 1000000]["aligned"].min()
+    subset_num = totalreads[totalreads["aligned"] >= lowread_filter]["aligned"].min()
     with open('qza_lowqualityfiltered.txt', 'w+') as fn:
         fn.write(qza)
     with open('readcount_maxsubset.txt', 'w+') as fn:
@@ -19,5 +19,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="""Merge together all taxonomy file output""")
     parser.add_argument("-q", "--qza_files", dest="qza", type=str, help="list of qza files")
     parser.add_argument("-r", "--readcount_totals", dest="readcount", type=str, help="list of sample readcounts")
+    parser.add_argument("-f", "--filter", dest="lowread_filter", type=int, help="samples lower than this user provided read count will be cut")
     args = parser.parse_args()
-    qiime_taxmerge(args.qza, args.readcount)
+    qiime_taxmerge(args.qza, args.readcount, args.lowread_filter)
