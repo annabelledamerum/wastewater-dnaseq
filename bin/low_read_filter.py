cc99df02d230b4209c1ddee4a59132186dd076bc
nsharp2 <nsharp@zymoresearch.com>
2023-06-23 12:32:20 -0700

    comments added to low_read_filter

--- bin/low_read_filter.py
+++ bin/low_read_filter.py
@@ -6,10 +6,14 @@ import argparse
 
 def qiime_taxmerge(qza, readcount, lowread_filter):
     totalreads = pd.read_csv(readcount, index_col=0)
+    #Remove all samples with low reads (default: 1 milliom)
     exclude = totalreads[totalreads["aligned"] < lowread_filter].index
+    # Remove all low read samples from qiime merge term
     for i in exclude:
         qza = re.sub(i+"_qiime_absfreq_table.qza", "", qza)
+    #next select the read count of the smallest sample that passed filtering. This number will be the maximum subset for diversity core analysis
     subset_num = totalreads[totalreads["aligned"] >= lowread_filter]["aligned"].min()
+    #stop diversity analysis if there are no samples that passed the total read cutoff filter
     if pd.notna(subset_num):
         with open('absqza_lowqualityfiltered.txt', 'w+') as fn:
             fn.write(qza)
