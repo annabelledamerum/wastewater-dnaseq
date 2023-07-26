943fa0614718f78c7aa601d32f4f4f476397078b
nsharp2 <nsharp@zymoresearch.com>
2023-07-26 16:41:52 -0700

    METADATAFILTER no longer removes samples that have no group or 1 group

--- bin/metadata_pairwise.r
+++ bin/metadata_pairwise.r
@@ -21,7 +21,7 @@ metadata <- metadata[metadata$sampleid %in% samplelist,]
 nums <- unlist(lapply(metadata, is.numeric))
 metadata <- metadata[ , !nums]
 #remove blanks or NA
-metadata <- metadata[!(is.na(metadata$group) | metadata$group==""), ]
+metadata$group <- ifelse((is.na(metadata$group) | metadata$group==""), metadata$sampleid, metadata$group)
 metadata <- metadata[,c("sampleid", "group")]
 #In this pipeline, multiple FASTQ files of the same sample can be merged tgoether; remove leftover duplicates from metadata
 metadata <- dplyr::distinct(metadata, sampleid, .keep_all=TRUE)
@@ -32,10 +32,8 @@ rownames(metadata) <- metadata$sample
 #Create grouplist and remove replicates from the list
 no.replicates <- table(metadata$group)
 groups <- names(no.replicates)[no.replicates > 1]
-#Keep groups with at least 2 replicates
-metadata <- metadata[metadata$group %in% groups,]
 #If more than three samples and more than one group, continue with group analysis
-if (length(metadata$sampleid > 3) & length(groups) > 1 )
+if (length(groups) >= 1 )
 {
     write.table(metadata, file="filtered_metadata.tsv", sep="\t", row.names=FALSE, quote=FALSE)
 }
