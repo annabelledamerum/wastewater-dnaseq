4000d9986ed91599ff663349ac2a5530df698d73
nsharp2 <nsharp@zymoresearch.com>
2023-12-19 14:13:47 -0800

    Method of calculating top 20 genes updated to mean percentage

--- bin/top20genes.py
+++ bin/top20genes.py
@@ -11,11 +11,10 @@ def top20genes(resistome_results):
     genelevel_resistome_mqc = {
         "id": "top20_genes_composition",
         "section_name": "Top 20 Genes Composition of Each Sample (AMRplusplus)",
-        "description": ("This plot depicts the composition of genes identified (or not) by "
+        "description": ("This plot depicts the composition of genes identified by "
             "<a href='https://github.com/Microbial-Ecology-Group/AMRplusplus/tree/master'>AMRplusplus</a>. "
             "It includes counts of anti-microbial resistant genes detected in each shotgun sample. "
-            "Gene categories in AMR are comprised from collections of  read counts matching antimicrobial resistance genes. "
-            "For each sample, the top 20 genes are demarcated by different colored bars. All other genes are labeled as 'Other' ."
+            "The top 20 mean percentage genes among all samples are demarcated by different colored bars. All other genes are labeled as 'Other' ."
             ),
         "plot_type": "bargraph",
         "anchor": "top20genes_amr_composition",
@@ -28,41 +27,31 @@ def top20genes(resistome_results):
             }
     }
 
-    AMRgene = pd.read_csv(resistome_results, index_col=0)
+    AMRgene = pd.read_csv("C:/Users/NSharp/Downloads/genes_AMR_analytic_matrix.csv", index_col=0)
     AMRgene = AMRgene.reindex(sorted(AMRgene.columns), axis=1)
-    genelist = defaultdict()
+    AMRperc = pd.DataFrame()
 
-    AMRgene["mean"] = AMRgene.mean(axis=1)
-    AMRgene = AMRgene.sort_values(by=["mean"], ascending = False)
-    AMRgene = AMRgene.drop(columns = ['mean'])
-    meanorder = list(AMRgene.index)
-    geneschosen = list()
+    samplecount_columns = AMRgene.columns
+    for column in samplecount_columns:
+        AMRperc[column] = (AMRgene[column]/sum(AMRgene[column]))*100
+                
+    AMRperc["mean"] = AMRperc.mean(axis=1)
+    AMRperc = AMRperc.sort_values(by=["mean"], ascending = False)
+    AMRgene = AMRgene.reindex(AMRperc.index, axis=0)
+    others = AMRgene.iloc[20:,:].sum(axis = 'rows')
+    others.name = "Others"
+    AMRgene = AMRgene.iloc[:20,:]
+    AMRgene = AMRgene.append(others)
 
-    for sample in AMRgene.columns:
-        samplesorted = dict(AMRgene.sort_values(by=[sample], ascending = False).iloc[:20,:][sample])
-        for key in samplesorted.keys():
-            if key not in geneschosen:
-                geneschosen.append(key)
-        samplesorted["Other"] = sum(AMRgene.sort_values(by=[sample], ascending = False).iloc[20:,:][sample])
-        genelist[sample] = samplesorted
-
-    geneorder = [x for x in meanorder if x in geneschosen]
-    colors = ['#a1c9f4','#b9f2f0','#ffb482','#cfcfcf','#fbafe4','#8de5a1','#029e73','#fab0e4','#949494','#ca9161','#d55e00','#d0bbff','#debb9b','#56b4e9','#0173b2','#de8f05','#ece133','#cc78bc','#ff9f9b', '#fffea3']
-    #Settings for colors
-    if len(geneorder) > 20:
-        color_21 = sns.color_palette("pastel", len(geneorder)-20).as_hex()
-        random.shuffle(color_21)
-        colors = colors + color_21
-    else:
-        colors = colors[0:len(geneorder)]
-
+    AMRgene_json = AMRgene.to_json()
+    geneorder = list(AMRgene.index)
+    colors = ['#a1c9f4','#b9f2f0','#ffb482','#cfcfcf','#fbafe4','#8de5a1','#029e73','#fab0e4','#949494','#ca9161','#d55e00','#d0bbff','#debb9b','#56b4e9','#0173b2','#de8f05','#ece133','#cc78bc','#ff9f9b', '#fffea3', '#999999']
     geneorder_dict = defaultdict()
-    for gene,color in zip(geneorder,colors):
-        geneorder_dict[gene] = {"color":color}
-    geneorder_dict["Other"] = {"color":"#999999"}
+        for gene,color in zip(geneorder,colors):
+            geneorder_dict[gene] = {"color":color}
 
     genelevel_resistome_mqc["categories"] = geneorder_dict
-    genelevel_resistome_mqc["data"] = genelist
+    genelevel_resistome_mqc["data"] = AMRgene_json
     with open('genelevel_resistomechart_mqc.json', 'w') as ofh:
         json.dump(genelevel_resistome_mqc, ofh, indent=4)
 
