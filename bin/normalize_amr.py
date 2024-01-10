2a1298fa6ed7fa89c0757d727747077b34de324a
nsharp2 <nsharp@zymoresearch.com>
2024-01-10 11:04:36 -0800

    comments added to python script normalize_amr.py

--- bin/normalize_amr.py
+++ bin/normalize_amr.py
@@ -7,21 +7,24 @@ import re
 import json
 
 def normalize_amr(rawcounts, flagstats):
+    #totalcountstable is intended to hold the # of properly paired reads per sample
     totalcountstable = pd.DataFrame()
     for sample in flagstats:
          samplename = re.sub("_flagstat.txt", "", sample)
          with open(sample, "r") as sampleinfo:
              infotext = sampleinfo.read()
-             print(infotext)
+             #extract # of properly paired reads from FASTQ samtools flagstat file
              properly_paired = re.search("(\d+) \+ \d+ paired in sequencing\n", infotext)
              properly_paired_num = int(properly_paired.group(1))/2
              totalcountstable.insert(len(totalcountstable.columns), samplename, [properly_paired_num])
 
     totalcountstable = totalcountstable.reindex(sorted(totalcountstable.columns), axis=1)
     for cat_type in rawcounts:
+        #get category name [class, mechanism, gene]
         cat_name = re.sub("_rawcounts_AMR_analytic_matrix.csv", "", cat_type)
         count_df = pd.read_csv(cat_type, index_col = "gene_accession")
         count_df = count_df.reindex(sorted(count_df.columns), axis=1)
+        #for each sample in count_df, scale to 1 million reads
         for sample in count_df.columns:
             count_df[sample] = (count_df[sample]/totalcountstable.loc[0,sample])*1000000
         count_df.to_csv(cat_name+"_normalized_AMR_analytic_matrix.csv")
