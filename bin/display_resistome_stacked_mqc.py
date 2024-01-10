b653f11506381b42a2100fdf41fbb927e0d05711
nsharp2 <nsharp@zymoresearch.com>
2024-01-10 10:56:25 -0800

    normalize amr script added to pipeline

--- bin/display_resistome_stacked_mqc.py
+++ bin/display_resistome_stacked_mqc.py
@@ -10,7 +10,8 @@ def parse_resistome(resistome_results):
         "section_name": "Resistome composition (AMRplusplus)",
         "description": ("This plot depicts the composition of reads of antimicrobial resistance gene classes identified (or not) by "
             "<a href='https://github.com/Microbial-Ecology-Group/AMRplusplus/tree/master'>AMRplusplus</a>. "
-            "It includes counts of anti-microbial resistant gene classes detected in each shotgun sample. "
+            "Reads have been normalized to counts per one million reads passing trimming filters for each sample."
+            "This plot includes counts of anti-microbial resistant gene classes detected in each shotgun sample. "
             "Each class contains multiple genes. The plotted read counts are the sum of read counts of all genes in that class. "
             "For gene level data, please refer to downloadable table gene_AMR_analytic_matrix. "
             "Percentages in this stacked bargraph represent the share of antimicrobial gene reads for a certain class over the " 
---
+++ bin/normalize_amr.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import pandas as pd
+import numpy as np
+import argparse
+import re
+import json
+
+def normalize_amr(rawcounts, flagstats):
+    totalcountstable = pd.DataFrame()
+    for sample in flagstats:
+         samplename = re.sub("_flagstat.txt", "", sample)
+         with open(sample, "r") as sampleinfo:
+             infotext = sampleinfo.read()
+             print(infotext)
+             properly_paired = re.search("(\d+) \+ \d+ paired in sequencing\n", infotext)
+             properly_paired_num = int(properly_paired.group(1))/2
+             totalcountstable.insert(len(totalcountstable.columns), samplename, [properly_paired_num])
+
+    totalcountstable = totalcountstable.reindex(sorted(totalcountstable.columns), axis=1)
+    for cat_type in rawcounts:
+        cat_name = re.sub("_rawcounts_AMR_analytic_matrix.csv", "", cat_type)
+        count_df = pd.read_csv(cat_type, index_col = "gene_accession")
+        count_df = count_df.reindex(sorted(count_df.columns), axis=1)
+        print(totalcountstable)
+        for sample in count_df.columns:
+            count_df[sample] = (count_df[sample]/totalcountstable.loc[0,sample])*1000000
+        count_df.to_csv(cat_name+"_normalized_AMR_analytic_matrix.csv")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="""display unaligned/aligned read statistics""")
+    parser.add_argument("-r", "--rawcounts", dest="rawcounts", nargs='+', type=str, help="string containing file titles for AMR Count statistics")
+    parser.add_argument("-f", "--flagstats", dest="flagstats", nargs='+', type=str, help="samtools flagstat on BAM files to get properly paired # of total reads")
+    args = parser.parse_args()
+    normalize_amr(args.rawcounts, args.flagstats)
